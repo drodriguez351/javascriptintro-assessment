a959dec2381895432c45209bca2fc7d98b7a4e54
David Rodriguez <twilightmetroid@gmail.com>
2023-07-05 12:55:06 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let firstName = "David";
+let lastName = "Rodriguez";
+let age = 16;
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("I am having a splendid time in this class making web pages.")
